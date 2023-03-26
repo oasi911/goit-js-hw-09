6bda3d40f61e20b5ced426cb4c096146a248cd4b
oasi911 <sokolanton.1998@gmail.com>
2023-03-26 23:33:07 +0300

    v0.3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,14 +2,11 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
-const currentDate = new Date();
-let timeDifference = 0;
+let pickedTime = null;
 function onClose(selectedDates) {
-  timeDifference = Math.floor(
-    (Date.parse(selectedDates[0]) - Date.parse(currentDate)) / 1000
-  );
+  pickedTime = selectedDates[0];
 
-  if (timeDifference > 0) {
+  if (options.defaultDate <= pickedTime) {
     startBtn.removeAttribute('disabled');
   } else {
     Notiflix.Notify.failure('Please choose a date in the future');
@@ -28,32 +25,38 @@ flatpickr('input#datetime-picker', options);
 
 const startBtn = document.querySelector('button[data-start]');
 startBtn.setAttribute('disabled', true);
+startBtn.addEventListener('click', startCounter);
 
-let timer = null;
-startBtn.addEventListener('click', () => {
+function startCounter() {
   const seconds = document.querySelector('span[data-seconds]');
   const minutes = document.querySelector('span[data-minutes]');
   const hours = document.querySelector('span[data-hours]');
   const days = document.querySelector('span[data-days]');
 
-  function addLeadingZero(num) {
-    return num < 10 ? '0' + num : num.toString();
-  }
+  let timer = null;
+  timer = setInterval(() => {
+    const currentDate = new Date();
+    const timeDifference = Math.floor(
+      (Date.parse(pickedTime) - Date.parse(currentDate)) / 1000
+    );
 
-  if (timeDifference > 0) {
-    timer = setInterval(() => {
-      timeDifference--;
+    days.textContent = addLeadingZero(
+      Math.floor(timeDifference / (60 * 60 * 24))
+    );
+    hours.textContent = addLeadingZero(
+      Math.floor((timeDifference % (60 * 60 * 24)) / (60 * 60))
+    );
+    minutes.textContent = addLeadingZero(
+      Math.floor((timeDifference % (60 * 60)) / 60)
+    );
+    seconds.textContent = addLeadingZero(Math.floor(timeDifference % 60));
 
-      days.textContent = addLeadingZero(
-        Math.floor(timeDifference / (60 * 60 * 24))
-      );
-      hours.textContent = addLeadingZero(
-        Math.floor((timeDifference % (60 * 60 * 24)) / (60 * 60))
-      );
-      minutes.textContent = addLeadingZero(
-        Math.floor((timeDifference % (60 * 60)) / 60)
-      );
-      seconds.textContent = addLeadingZero(Math.floor(timeDifference % 60));
-    }, 1000);
-  }
-});
+    if (timeDifference <= 0) {
+      clearInterval(timer);
+    }
+  }, 1000);
+}
+
+function addLeadingZero(num) {
+  return num < 10 ? '0' + num : num.toString();
+}
